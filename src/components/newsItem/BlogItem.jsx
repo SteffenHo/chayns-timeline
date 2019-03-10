8d95f7b3f6bf3211278f148c53ad663ea59b3510
Steffen Holtkamp <stef.holt@yahoo.de>
2019-03-10 18:37:22 +0100

    Rename newsitem

--- src/components/newsItem/BlogItem.jsx
+++ src/components/newsItem/BlogItem.jsx
@@ -1,9 +1,17 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Gallery from 'chayns-components/lib/react-chayns-gallery/component/Gallery';
+import TextTruncate from 'react-text-truncate';
 
 
 class NewsItem extends PureComponent {
+
+    selectTapp = (id) => {
+        console.log('click');
+        chayns.selectTapp({
+            'id': 91958
+        }, "M=" + id);
+    }
     render() {
         const {
             headline,
@@ -12,11 +20,18 @@ class NewsItem extends PureComponent {
             images
         } = this.props;
         const imgList = images.map(url => ({ url }));
+
+        const readMore = (<a onClick={() => this.selectTapp(postingId)}>Mehr anzeigen</a>);
         console.log('imgList', imgList, description);
     return (
             <div>
                <h1>{headline}</h1>
-                <div>{description}</div>
+                <TextTruncate
+                    line={5}
+                    truncateText="…"
+                    text={description}
+                    textTruncateChild={readMore}
+                />
                 {imgList.size > 0 ?
                     <Gallery images={imgList.toJS()}/>
                     : ''
