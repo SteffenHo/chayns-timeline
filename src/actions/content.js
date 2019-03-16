51ca4c6a9252d52b91f4bf4659f0a6b4566f4887
Steffen Holtkamp <stef.holt@yahoo.de>
2019-03-16 22:56:21 +0100

    Add function to save and load blogs

--- src/actions/content.js
+++ src/actions/content.js
@@ -1,5 +1,7 @@
-import { fromJS } from 'immutable';
-import { getEvents, getNews } from '../api';
+import { fromJS, List } from 'immutable';
+import {
+ getEvents, getNews, loadBlogs, postBlog, postBlogs
+} from '../api';
 
 export const GET_NEWS_DATA = 'GET_NEWS_DATA';
 export const getNewsData = data => ({
@@ -8,7 +10,7 @@ export const getNewsData = data => ({
 });
 
 export const getMashupNews = (timestamp = Date.now()) => (dispatch) => {
-    getNews(timestamp).then((data) => dispatch(getNewsData(fromJS(data).get('itemList'))));
+    getNews(timestamp).then(data => dispatch(getNewsData(fromJS(data).get('itemList'))));
 };
 
 export const GET_EVENTS_DATA = 'GET_EVENTS_DATA';
@@ -18,7 +20,7 @@ export const getEventsData = data => ({
 });
 
 export const getMashupEvents = (skip = 0, take = 10) => (dispatch) => {
-    getEvents(skip, take).then((data) => dispatch(getEventsData(fromJS(data))));
+    getEvents(skip, take).then(data => dispatch(getEventsData(fromJS(data))));
 };
 
 export const SET_BLOG_DATA = 'SET_BLOG_DATA';
@@ -26,8 +28,18 @@ export const setBlogData = data => ({
     type: SET_BLOG_DATA,
     data
 });
-export const saveBlogElement = (element) => (dispatch, getState) => {
-    const {content} = getState();
+export const saveBlogElement = element => (dispatch, getState) => {
+    const { content } = getState();
     console.log('saveBlogElement', element, content);
-    dispatch(setBlogData(content.get('blogs').push(element)));
+    const allBlogs = content.get('blogs').push(element);
+    postBlogs(allBlogs).then(data => dispatch(setBlogData(allBlogs)));
+};
+
+export const getBlogs = () => (dispatch) => {
+    loadBlogs().then((data) => {
+        if(data != null) {
+            const blogsData = fromJS(data);
+            dispatch(setBlogData(blogsData || new List()));
+        }
+    });
 };
